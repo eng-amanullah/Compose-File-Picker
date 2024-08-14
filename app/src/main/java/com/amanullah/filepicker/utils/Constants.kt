95e4f8adeb2588db089e1363dd65f307fc87cee4
amanullah <amanullah.sarker@amiprobashi.com>
2024-08-14 22:05:34 +0600

    feature : Constants
    changes :
    - add Constants

--- app/src/main/java/com/amanullah/filepicker/utils/Constants.kt
+++ app/src/main/java/com/amanullah/filepicker/utils/Constants.kt
@@ -5,5 +5,11 @@ class Constants {
         const val CAMERA = 1
         const val GALLERY = 2
         const val FILE = 3
+
+        const val SINGLE_ITEM = "single_item"
+        const val MULTIPLE_ITEM = "multiple_item"
+
+        const val DIALOG = "dialog"
+        const val BOTTOM_SHEET = "bottom_sheet"
     }
 }
