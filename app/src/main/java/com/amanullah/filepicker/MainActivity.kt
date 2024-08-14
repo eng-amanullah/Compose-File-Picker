139154df0d51f016a9810bbaa85cb251ea479490
amanullah <amanullah.sarker@amiprobashi.com>
2024-08-14 20:31:49 +0600

    feature : Main Activity
    changes :
    - add gallery support
    - showing image picked from galley, file or camera
    - optimize code

--- app/src/main/java/com/amanullah/filepicker/MainActivity.kt
+++ app/src/main/java/com/amanullah/filepicker/MainActivity.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,9 +20,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.amanullah.filepicker.customview.ChooseFromGalleryAndCamera
 import com.amanullah.filepicker.customview.CustomDialog
+import com.amanullah.filepicker.customview.DisplayImageFromFile
 import com.amanullah.filepicker.ui.theme.FilePickerTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,17 +34,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             FilePickerTheme {
 
-                var showDialog by remember { mutableStateOf(false) }
+                var showDialog by remember { mutableStateOf(value = false) }
+                var imageFile by remember { mutableStateOf<File?>(value = null) }
 
                 Scaffold(
                     modifier = Modifier
                         .fillMaxSize()
                 ) { innerPadding ->
-                    Box(
+                    Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(paddingValues = innerPadding),
-                        contentAlignment = Alignment.Center
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
                     ) {
                         if (showDialog) {
                             CustomDialog(
@@ -47,13 +54,12 @@ class MainActivity : ComponentActivity() {
                                 enableOutSideClick = false,
                                 content = {
                                     ChooseFromGalleryAndCamera(
-                                        galleryButtonCallBack = { file ->
-                                            Log.e("GALLERY_BUTTON_CALL_BACK", file.toString())
+                                        buttonCallBack = { file ->
+                                            Log.e("CALL_BACK", file.toString())
 
-                                            showDialog = false
-                                        },
-                                        cameraButtonCallBack = { file ->
-                                            Log.e("CAMERA_BUTTON_CALL_BACK", file.toString())
+                                            imageFile = if (!file.toString()
+                                                    .contains(other = ".pdf")
+                                            ) file else null
 
                                             showDialog = false
                                         },
@@ -67,6 +73,15 @@ class MainActivity : ComponentActivity() {
                                 }
                             )
                         } else {
+                            imageFile?.let {
+                                DisplayImageFromFile(
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .padding(bottom = 16.dp),
+                                    file = it
+                                )
+                            }
+
                             Button(
                                 onClick = {
                                     showDialog = true
