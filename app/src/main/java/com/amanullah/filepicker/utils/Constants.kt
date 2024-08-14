6096eeff934e9f1119ffc60165b08b2cfa31b1dc
amanullah <amanullah.sarker@amiprobashi.com>
2024-08-14 20:29:24 +0600

    feature : Constants
    changes :
    - add constant for different type

---
+++ app/src/main/java/com/amanullah/filepicker/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.amanullah.filepicker.utils
+
+class Constants {
+    companion object {
+        const val CAMERA = 1
+        const val GALLERY = 2
+        const val FILE = 3
+    }
+}
